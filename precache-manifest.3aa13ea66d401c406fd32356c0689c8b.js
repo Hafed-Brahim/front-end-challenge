7c4b18d3ba4d03d2f86c463f89946881238210f3
hafed-brahim <hafed.brh@gmail.com>
2020-01-11 03:40:07 +0100

    Updates

---
+++ precache-manifest.3aa13ea66d401c406fd32356c0689c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3617473f1ba50d24bad7bb8ef967178b",
+    "url": "/front-end-challenge/index.html"
+  },
+  {
+    "revision": "350bb1c23d4ea7a538d5",
+    "url": "/front-end-challenge/static/css/main.3fa0fd30.chunk.css"
+  },
+  {
+    "revision": "43796d877339d77abf5f",
+    "url": "/front-end-challenge/static/js/2.6db5eabb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/front-end-challenge/static/js/2.6db5eabb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "350bb1c23d4ea7a538d5",
+    "url": "/front-end-challenge/static/js/main.568c640d.chunk.js"
+  },
+  {
+    "revision": "f853368e142731662d6d",
+    "url": "/front-end-challenge/static/js/runtime-main.c83b8d59.js"
+  }
+]);
